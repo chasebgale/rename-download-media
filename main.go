45cf3ad3f872e8937eb0b56694c5c12f751ef369
Chase Brandon Gale <chase.b.gale@gmail.com>
2026-02-09 13:36:39 -0800

    comments and slightly more functional

--- main.go
+++ main.go
@@ -11,34 +11,37 @@ import (
 
 func main() {
 
+	// install.reg initializes the context menu command to call this program with the path of the folder that was right-clicked on as the first argument
     fullPath := os.Args[1]
-
-    fmt.Println(fullPath)
-
-	dateFinderRegex := regexp.MustCompile(`\d{4}\.`)
+    fmt.Println("path: ", fullPath)
 
 	folderName := folderName(fullPath)
-	fmt.Println("folderName:", folderName)
+	fmt.Println("folder: ", folderName)
 
-	dateIdx := dateFinderRegex.FindStringIndex(folderName)
+	dateIdx := dateIndexFromFolderName(folderName)
 	if (dateIdx == nil) {
 		displayErrorAndWait("No date found in folder name.", nil)
 		return
 	}
-	fmt.Println("idx:", dateIdx)
 
+	// Format the preferred film name as "Film Name (Year)" by using the date found in the folder name and removing any periods
+	// (This is the format Jellyfin expects)
+	// TODO: Perhaps scan original name for dashes and periods and replace whichever is more common, as some rare files use dashes over periods
 	filmWithPeriods := folderName[:dateIdx[0]] + `(` + folderName[dateIdx[0]:dateIdx[1]-1] + `)`
-	film := regexp.MustCompile(`\.`).ReplaceAllString(filmWithPeriods, " ")
-	fmt.Println("film:", film)
+	filmName := regexp.MustCompile(`\.`).ReplaceAllString(filmWithPeriods, " ")
+	fmt.Println("film:", filmName)
 	
-	err := findAndRenameFilm(fullPath, film)
+	// Rename the film file with our formatted name
+	// TODO: Upate func to only rename largest media file found to account for sample files, etc
+	err := findAndRenameFilm(fullPath, filmName)
 	if err != nil {
 		displayErrorAndWait("Error renaming film file.", err)
 		return
 	}
 
-	fmt.Println("Renaming folder: ", fullPath, " to ", filepath.Join(filepath.Dir(fullPath), film))
-	err = os.Rename(fullPath, filepath.Join(filepath.Dir(fullPath), film))
+	// Rename the folder with our formatted name
+	fmt.Println("Renaming folder: ", fullPath, " to ", filepath.Join(filepath.Dir(fullPath), filmName))
+	err = os.Rename(fullPath, filepath.Join(filepath.Dir(fullPath), filmName))
 	if (err != nil) {
 		displayErrorAndWait("Error renaming film folder.", err)
 		return
@@ -88,6 +91,11 @@ func findAndRenameFilm(path string, formattedFilmName string) error {
 func folderName(path string) string {
 	pathParts := regexp.MustCompile(`\\`).Split(path, -1)
 	return pathParts[len(pathParts)-1]
+}
+
+func dateIndexFromFolderName(folderName string) []int {
+	dateFinderRegex := regexp.MustCompile(`\d{4}\.`)
+	return dateFinderRegex.FindStringIndex(folderName)
 }
 
 func displayErrorAndWait(message string, err error) {
